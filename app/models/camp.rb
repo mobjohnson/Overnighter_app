0a05af14e0fb90ccbb9a4057f6ad20cb2a070fd4
Martin Johnson <mobjohnson@gmail.com>
2014-07-03 11:19:44 -0400

    Remove comments and rating now working

--- app/models/camp.rb
+++ app/models/camp.rb
@@ -9,11 +9,10 @@ class Camp < ActiveRecord::Base
 	validates :location, length: {minimum: 8}
 	validates :facilities, length: {minimum: 5}
 	validates :terrain, length: {minimum: 4}
-	validates :rating, numericality: {greater_than: 0 }
-	validates :rating, numericality: {less_than: 6 }
-	validates :comments, length: {minimum: 10}
+	validates :activities, presence: true
+	validates :fires, presence: true
 
 	def self.search_camp(param)
-		where("name LIKE :query OR location LIKE :query OR terrain LIKE :query", query: "%#{param}%")
+		where("name LIKE :query OR location LIKE :query OR terrain LIKE :query OR activities LIKE :query", query: "%#{param}%")
 	end
 end
