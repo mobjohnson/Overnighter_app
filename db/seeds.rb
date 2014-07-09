1489d0cd192fc277c30dda8e58373de2d6abb829
Martin Johnson <mobjohnson@gmail.com>
2014-07-09 19:26:01 -0400

    Updated prod config.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,14 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 puts "Adding a Whole bunch of camps"
 Camp.create name: "Burke Lake", address: "Burke Lake Park, Fairfax Station, VA", facilities: "Showers/Bathrooms", terrain: "Flat", fires: "Permitted", activities: "Canoeing"
+sleep(1)
 Camp.create name: "Pohick Bay", address: "Pohick Bay Park, Lorton, VA", facilities: "Showers/Bathrooms", terrain: "Flat", fires: "Permitted", activities: "Canoeing"
+sleep(1)
 Camp.create name: "Prince William Forest", address: "Prince William Forest Park, 18100 Park Headquarters Rd, Triangle, VA", facilities: "Bathrooms", terrain: "Hilly", fires: "Permitted", activities: "Hiking"
+sleep(1)
 Camp.create name: "Hogback Mountain", address: "Hogback Mountain, South River, VA", facilities: "Primitive", terrain: "Mountaneous", fires: "Permitted", activities: "Canoeing", fires: "Permitted"
+sleep(1)
 Camp.create name: "Shenandoah River Outfitters", address: "Shenandoah River Outfitters, 6502 S Page Valley Rd, Luray, VA", facilities: "Showers/Bathrooms", terrain: "Hilly", fires: "Permitted", activities: "Canoeing"
+sleep(1)
 Camp.create name: "Camp Rock Enon", address: "Camp Rock Enon, 292 Rock Enon Springs Rd, Gore, VA", facilities: "Showers/Bathrooms", terrain: "Mountaneous", fires: "Permitted", activities: "Canoeing"
  
