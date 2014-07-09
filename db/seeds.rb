586bc745b4c35c15845057a18e99541f7fcbe0d0
Martin Johnson <mobjohnson@gmail.com>
2014-07-09 16:08:03 -0400

    Pretty it up

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 puts "Adding a Whole bunch of camps"
-Camp.create name: "Burke Lake Park", location: "Fairfax Station, VA", facilities: "Showers/Bathrooms", terrain: "Flat", rating: 3, comments: "Nice place, but too many Cub Scouts!",latitude: 38.760835,longitude: -77.301629 
-Camp.create name: "Pohick Bay", location: "Lorton, VA", facilities: "Showers/Bathrooms", terrain: "Flat", rating: 3, comments: "Nice place, but too many Cub Scouts!",latitude: 38.672738,longitude: -77.172646  
-Camp.create name: "Prince William Forrest", location: "Triangle, VA", facilities: "Bathrooms", terrain: "Hilly", rating: 3, comments: "Awesome Orienteering course",latitude: 38.585278,longitude: -77.379722
-Camp.create name: "Hogback Mountain", location: "South River, VA", facilities: "Primitive", terrain: "Mountaneous", rating: 5, comments: "Can't wait to go back!",latitude: 38.7620613,longitude: -78.2738911 
-Camp.create name: "Shenandoah River Outfitters", location: "Luray, VA", facilities: "Showers/Bathrooms", terrain: "Hilly", rating: 4, comments: "Nice Class III rapids!",latitude: 38.756367,longitude: 38.756367 
-Camp.create name: "Camp Rock Enon", location: "Gore, VA", facilities: "Showers/Bathrooms", terrain: "Mountaneous", rating: 4, comments: "Best place for a Frezze-a-Ree!",latitude: 39.214223,longitude: -78.385657
+Camp.create name: "Burke Lake", address: "Burke Lake Park, Fairfax Station, VA", facilities: "Showers/Bathrooms", terrain: "Flat", fires: "Permitted", activities: "Canoeing"
+Camp.create name: "Pohick Bay", address: "Pohick Bay Park, Lorton, VA", facilities: "Showers/Bathrooms", terrain: "Flat", fires: "Permitted", activities: "Canoeing"
+Camp.create name: "Prince William Forest", address: "Prince William Forest Park, 18100 Park Headquarters Rd, Triangle, VA", facilities: "Bathrooms", terrain: "Hilly", fires: "Permitted", activities: "Hiking"
+Camp.create name: "Hogback Mountain", address: "Hogback Mountain, South River, VA", facilities: "Primitive", terrain: "Mountaneous", fires: "Permitted", activities: "Canoeing", fires: "Permitted"
+Camp.create name: "Shenandoah River Outfitters", address: "Shenandoah River Outfitters, 6502 S Page Valley Rd, Luray, VA", facilities: "Showers/Bathrooms", terrain: "Hilly", fires: "Permitted", activities: "Canoeing"
+Camp.create name: "Camp Rock Enon", address: "Camp Rock Enon, 292 Rock Enon Springs Rd, Gore, VA", facilities: "Showers/Bathrooms", terrain: "Mountaneous", fires: "Permitted", activities: "Canoeing"
  
